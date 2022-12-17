e628a52748f68b49d8cd5765c7756f1ac0cfe586
Daisuke <nakada0907@outlook.com>
2022-12-17 15:48:50 +0900

    fix echo

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -5,6 +5,6 @@ dpkg -s emacs-mozc &> /dev/null
 if [ $? -eq 0 ]; then
     ln -s ~/dotfiles/init.el ~/.emacs.d/init.el
 else
-    echo "emacs-mozc-bin is Not installed!"
+    echo "emacs-mozc is Not installed!"
     exit 1
 fi
