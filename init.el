8df8c9c6565bf13e05f35f0da486011b2135038b
Daisuke <nakada0907@outlook.com>
2023-01-06 23:04:41 +0900

    add magit and change theme

--- init.el
+++ init.el
@@ -2,14 +2,26 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(package-install 'julia-mode)
+(unless (package-installed-p 'julia-mode)
+  (package-install 'julia-mode)
+  )
 (require 'julia-mode)
+
+(unless (package-installed-p 'magit)
+  (package-install 'magit)
+  )
+(require 'magit)
+
+(column-number-mode 1)
+
+(load-theme 'tango-dark t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(cmake-mode julia-mode)))
+ '(package-selected-packages '(cmake-mode magit julia-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
