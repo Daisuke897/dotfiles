432a07b69bbae9cae4d7037879882b9bf154e322
Daisuke <nakada0907@outlook.com>
2023-09-14 20:49:15 +0900

    Added keys related to counsel

--- init.el
+++ init.el
@@ -30,6 +30,8 @@
   (setq ivy-use-virtual-buffers t)
   (setq ivy-count-format "(%d/%d) ")
   (global-set-key "\C-s" 'swiper)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "C-c g") 'counsel-git)
   )
 
 (use-package counsel
