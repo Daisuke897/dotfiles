45aac25ec2fa27bc25262f93f3963f9ad9f164b1
Daisuke <nakada0907@outlook.com>
2025-06-26 20:26:22 +0900

    feat: add flake.nix for cross-platform Emacs + Home Manager config

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Daisuke’s cross-platform Emacs + Home-Manager flake";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-25.05";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    systems = [
+      { name = "linux"; system = "x86_64-linux"; homeDir = "/home/daisuke"; }
+      { name = "macos"; system = "aarch64-darwin"; homeDir = "/Users/daisuke"; }
+    ];
+
+    mkHome = { name, system, homeDir }: let
+      pkgs = import nixpkgs { inherit systems; };
+    in
+      home-manager.lib.homeManagerConfiguration {
+        inherit pkgs system;
+        home.username = "daisuke";
+        home.homeDirectory = homeDir;
+
+        modules = [
+          ({ config, pkgs, ...}: {
+            programs.emacs = {
+              enable = true;
+              package = pkgs.emacs;
+              extraConfig = builtins.readFile ./init.el;
+            };
+            home.file.".emacs.d/init.el".source = ./init.el;
+          })
+        ];
+      };
+
+      homeConfigs = builtins.listToAttrs (map (s:
+        { name = "daisuke-${s.name}";
+          value = mkHome s;}
+      ) systems);
+  in
+    {
+    };
+}
