a7db80c72d7cf85677a671c196551c8e7a051eca
Daisuke <nakada0907@outlook.com>
2025-06-29 17:01:11 +0900

    feat(devShells): add LSP wrappers for TypeScript, Vue, Astro, ESLint, and CSS
    - Introduced `mkLspWrapper` helper to declaratively install npm-based LSP servers into user-local cache directories
    - Added support for:
      - typescript-language-server with Vue and Astro plugins
      - @vue/language-server (Volar)
      - @astrojs/language-server
      - vscode-eslint-language-server
      - vscode-css-language-server
    - Replaced direct use of nodePackages.typescript-language-server with wrapped version
    - Fixed incorrect `inherit systems` in `mkHome` to `inherit system`

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
     ];
 
     mkHome = { name, system, homeDir }: let
-      pkgs = import nixpkgs { inherit systems; };
+      pkgs = import nixpkgs { inherit system; };
     in
       home-manager.lib.homeManagerConfiguration {
         inherit pkgs system;
@@ -39,6 +39,25 @@
         ];
       };
 
+      mkLspWrapper = { pkgs, name, npmPackages, bin }: pkgs.writeShellApplication {
+        inherit name;
+        runtimeInputs = [ pkgs.nodejs pkgs.git ];
+        text = ''
+          CACHE_DIR="$HOME/.cache/${name}"
+          export NODE_PATH="$CACHE_DIR/node_modules"
+          export PATH="$CACHE_DIR/node_modules/.bin:$PATH"
+
+          if [ ! -d "$CACHE_DIR/node_modules" ]; then
+            echo "Installing ${name} into $CACHE_DIR..."
+            mkdir -p "$CACHE_DIR"
+            cd "$CACHE_DIR"
+            npm install --no-save ${builtins.concatStringsSep " \\\n  " npmPackages}
+          fi
+
+          exec ${bin} "$@"
+        '';
+      };
+
       homeConfigs = builtins.listToAttrs (map (s:
         { name = "daisuke-${s.name}";
           value = mkHome s;}
@@ -50,11 +69,68 @@
       devShells = builtins.listToAttrs (map (s:
         let pkgs = import nixpkgs { inherit (s) system; };
 
+        typescriptLsp = mkLspWrapper {
+          inherit pkgs;
+          name = "typescript-lsp";
+          npmPackages = [
+            "typescript"
+            "typescript-language-server"
+            "@vue/typescript-plugin"
+            "@astrojs/ts-plugin"
+          ];
+          bin = "typescript-language-server";
+        };
+
+        vueLsp = mkLspWrapper {
+          inherit pkgs;
+          name = "vue-lsp";
+          npmPackages = [ "@vue/language-server" ];
+          bin = "vue-language-server";
+        };
+
+        astroLsp = mkLspWrapper {
+          inherit pkgs;
+          name = "astro-lsp";
+          npmPackages = [ "@astrojs/language-server" ];
+          bin = "astro-ls";
+        };
+
+        eslintLsp = mkLspWrapper {
+          inherit pkgs;
+          name = "eslint-lsp";
+          npmPackages = [ "vscode-langservers-extracted" ];
+          bin = "vscode-eslint-language-server";
+        };
+
+        cssLsp = mkLspWrapper {
+          inherit pkgs;
+          name = "css-lsp";
+          npmPackages = [ "vscode-langservers-extracted" ];
+          bin = "vscode-css-language-server";
+        };
+
         emacsLspDeps = with pkgs; [
           emacs
 
           nodejs
-          nodePackages.typescript-language-server
+
+          # Typescript
+          typescriptLsp
+
+          # Eslint
+          eslintLsp
+
+          # CSS
+          cssLsp
+
+          # Vue
+          vueLSP
+
+          # Astro
+          astroLsp
+
+          # YAML
+          nodePackages.yaml-language-server
 
           python3
           python3Packages.python-lsp-server
