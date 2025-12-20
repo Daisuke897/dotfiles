94a542426dc87ce8820d78f6545416555e4c1fb3
Daisuke <nakada0907@outlook.com>
2025-12-20 11:15:13 +0900

    chore: fix syntax by adding missing semicolon in flake.ni

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       systems = [ "x86_64-linux" "aarch64-darwin" ];
       perSystem = { config, pkgs, system, ... }: {
         packages = {
-          typescript-language-server = pkgs.typescript-language-server
+          typescript-language-server = pkgs.typescript-language-server;
         };
       };
     };
