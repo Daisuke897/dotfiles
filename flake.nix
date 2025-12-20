fd80f03961da8a0ebf44fad76b753157e7d60dcf
Daisuke <nakada0907@outlook.com>
2025-12-20 12:09:27 +0900

    feat(devShell): add common language servers to devShell buildInputs

--- flake.nix
+++ flake.nix
@@ -7,10 +7,31 @@
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-darwin" ];
-      perSystem = { config, pkgs, system, ... }: {
-        packages = {
-          default = pkgs.hello;
-          typescript-language-server = pkgs.typescript-language-server;
+      perSystem = { config, pkgs, system, ... }: let
+        devInputs = with pkgs; [
+          typescript-language-server
+          vscode-langservers-extracted
+          yaml-language-server
+          vue-language-server
+          astro-language-server
+          texlab
+          rust-analyzer
+          fortls
+          ruff
+          pyright
+          marksman
+        ];
+
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = devInputs;
+          shellHook = ''
+            # init.el をプロジェクトのものにリンク（上書きしない）
+            if [ ! -e "$HOME/.emacs.d/init.el" ]; then
+              mkdir -p $HOME/.emacs.d
+              ln -s ${toString ./init.el} $HOME/.emacs.d/init.el
+            fi
+          '';
         };
       };
     };
