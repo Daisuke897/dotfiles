f581727a0ef003085d9adebde77da1f6e14682ce
Daisuke <nakada0907@outlook.com>
2025-12-20 11:11:50 +0900

    feat: add initial flake.nix with flake-parts and TypeScript LSP package

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      perSystem = { config, pkgs, system, ... }: {
+        packages = {
+          typescript-language-server = pkgs.typescript-language-server
+        };
+      };
+    };
+}
