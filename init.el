1e74fbb80fe69fe8990c07951bb10618bb2d91dd
Daisuke <nakada0907@outlook.com>
2026-01-10 20:06:20 +0900

    chore: switch theme from modus-vivendi to modus-vivendi-tinted

--- init.el
+++ init.el
@@ -632,7 +632,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
- '(custom-enabled-themes '(modus-vivendi))
+ '(custom-enabled-themes '(modus-vivendi-tinted))
  '(gc-cons-threshold 100000000)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
